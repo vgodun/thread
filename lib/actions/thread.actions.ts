cc60da0cb07c419e6b2dc89356a80180e53fbd02
vgodun <v.godunsoftware@gmail.com>
2023-10-19 15:51:27 +0300

    add components comment

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -28,11 +28,126 @@ export async function createThread({text,author,communityId,path}:Params){
             $push: { threads: createdThread._id },
         });
 
-        revalidatePath(path);
+        revalidatePath(path)
 
     }
     catch (error: any) {
         throw new Error(`Failed to create thread: ${error.message}`);
     };
     
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    // Calculate the number of posts to skip based on the page number and page size.
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: "desc" })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({
+            path: "author",
+            model: User,
+        })
+        .populate({
+            path: "children", // Populate the children field
+            populate: {
+                path: "author", // Populate the author field within children
+                model: User,
+                select: "_id name parentId image", // Select only _id and username fields of the author
+            },
+        });
+
+    // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+    const totalPostsCount = await Thread.countDocuments({
+        parentId: { $in: [null, undefined] },
+    }); // Get the total count of posts
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+}
+
+export async function fetchThreadById(id:string){
+    connectToDB();
+
+    try {
+//TODO: Populate Community
+
+        const thread= await Thread.findById(id)
+        .populate({
+            path: 'author',
+            model: User,
+            select: '_id id name image',
+        })
+        .populate({
+            path: 'children',
+            populate:[
+                {
+                    path: 'author',
+                    model: User,
+                    select: '_id id name parentId image'
+                },
+                {
+                    path: 'children',
+                    model: Thread,
+                    populate: {
+                        path: 'author',
+                        model: User,
+                        select: '_id id name parentId image'
+                    }
+                }
+            ]
+        }).exec();
+
+        return thread;
+    } catch (error:any) {
+        throw new Error(`Failed to fetch thread: ${error.message}`);
+    }
+}
+
+export async function addCommentToThread(
+    threadId:string,
+    commentText:string,
+    userId:string,
+    path:string
+){
+    connectToDB();
+
+    try{
+        //Find the original thread by its ID
+
+        const originalThread = await Thread.findById(threadId);
+
+        if(!originalThread){
+            throw new Error('Thread not found');
+        }
+        //Create a new thread with the comment text
+
+        const commentThread= new Thread({
+            text: commentText,
+            author:userId,
+            parentId: threadId,
+        });
+
+        // Save the new thread
+
+        const savedCommentThread = await commentThread.save();
+
+        originalThread.children.push(savedCommentThread._id);
+
+        // Save the original thread
+
+        await originalThread.save();
+
+        revalidatePath(path);
+    }
+    catch(error:any){
+        throw new Error(`Failed to adding comment to thread: ${error.message}`);
+    }
 }
